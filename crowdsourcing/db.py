47d2318525f2aa815f6e8662e62c7808037825d6
DesMy <toscha.subbotin2014@yandex.ru>
2020-12-29 09:24:37 +0300

    rm replaced files

--- crowdsourcing/db.py
+++
@@ -1,6 +0,0 @@
-from neomodel import config
-
-
-def init_db():
-    config.DATABASE_URL = 'neo4j://neo4j:@neo4j:7687'
-
--- crowdsourcing/models.py
+++
@@ -1,9 +0,0 @@
-from neomodel import (
-    StructuredNode,
-    StringProperty
-)
-
-
-class Message(StructuredNode):
-    greeting_message = StringProperty(unique_index=True, required=True)
-
